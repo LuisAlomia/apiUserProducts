f3eb9afd738838ad9e4e97c2b96e88cf932dcdec
Luis Alomia <luisfidel44@gmail.com>
2023-01-14 18:49:10 -0500

    chore: configuration data base postgres with ORM sequelize

---
+++ src/database/connectDb.ts
@@ -0,0 +1,23 @@
+import { Sequelize } from "sequelize";
+
+import { config } from "../config/env.config";
+
+export const sequelize = new Sequelize({
+  username: config.db.username,
+  password: config.db.password,
+  database: config.db.database,
+  host: config.db.host,
+  port: config.db.port,
+  dialect: "postgres",
+});
+
+export const databaseRun = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("CONNECT");
+    await sequelize.sync();
+    console.log("SYNC");
+  } catch (err) {
+    console.log(err);
+  }
+};

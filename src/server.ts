0ca7716c71a3687d63955edc22f2738e46c73e96
Luis Alomia <luisfidel44@gmail.com>
2023-01-15 00:05:29 -0500

    refactor: add route Products

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import cors from "cors";
 import { config } from "./config/env.config";
 import { databaseRun } from "./database/connectDb";
 import { router as userRouter } from "./users/user.route";
+import { router as productRouter } from "./products/product.route";
 
 const app: Application = express();
 const port: number | string = config.port;
@@ -14,6 +15,7 @@ app.use(cors());
 
 //Routes
 app.use("/users", userRouter);
+app.use("/products", productRouter);
 
 databaseRun();
 
